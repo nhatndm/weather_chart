1b2c0e6675ac14edca3069227fef746acb7c8434
nhatndm <nhatndm1193@gmail.com>
2019-11-05 10:37:21 +0700

    init redux store, redux thunk, bootstrap for responsive

---
+++ src/redux/index.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const rootReducer = combineReducers({});
+
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);

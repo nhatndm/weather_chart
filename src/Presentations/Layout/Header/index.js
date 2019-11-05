364d1205a0ea3400b4a293501148c439bd6ffa32
nhatndm <nhatndm1193@gmail.com>
2019-11-05 22:06:34 +0700

    completed header

--- src/Presentations/Layout/Header/index.js
+++ src/Presentations/Layout/Header/index.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
+import { ReactComponent as CloudyIcon } from "../../../Assets/cloudy.svg";
+import { ReactComponent as SelectLanguageIcon } from "../../../Assets/planet-earth.svg";
+
 import "./index.scss";
 
 export default class HeaderPresentation extends Component {
   render() {
-    return <div className="app-header">This is header</div>;
+    return (
+      <div className="app-header">
+        <div className="container header-wrapper">
+          <div className="row select-language-wrapper">
+            <div className="col-12">
+              <SelectLanguageIcon className="icon-20" />
+              <p className="select-language">Select Language</p>
+            </div>
+          </div>
+          <div className="row brand-wrapper">
+            <div className="col-md-4 col-sm-12 header-icon-wrapper">
+              <CloudyIcon className="icon-64" />
+              <p className="web-title">Weather</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
